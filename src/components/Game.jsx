01a0143608970c361e846244183bbb7e6ed946d3
Daniel Amado <daniel.amadove@gmail.com>
2024-07-22 16:53:41 -0500

    Juego terminado con funcionalidad, falta refactorizar

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,7 +4,6 @@ import Popup from "./Popup";
 
 import { useState, useEffect } from "react";
 
-
 // Función para mezclar el array
 const shuffleArray = (array) => {
   let shuffledArray = [...array];
@@ -15,30 +14,49 @@ const shuffleArray = (array) => {
   return shuffledArray;
 };
 
-
-
 const Game = () => {
   const [modalisOpen, setModalIsOpen] = useState(false);
-  const [contenidoModal, setContenidoModal] = useState("Primero escoge una pregunta");
-  const [pregunta, setPregunta] = useState("");
-  const [respuesta, setRespuesta] = useState("");
-
   const [preguntasAleatorias, setPreguntasAleatorias] = useState([]);
+  const [contenidoModal, setContenidoModal] = useState("");
+  const [pregunta, setPregunta] = useState(null);
+  const [respuesta, setRespuesta] = useState(null);
+  const [preguntasResueltas, setPreguntasResueltas] = useState([]);
 
+  useEffect(() => {
+    const preguntasResueltasStorage =
+      JSON.parse(localStorage.getItem("preguntasResueltas")) || [];
+    setPreguntasResueltas(preguntasResueltasStorage);
+  }, []);
 
-
-  const selectRespuesta = (item) => {
-    setRespuesta(item.respuesta);
-  }
-
-  const selectPregunta = (item) => {
-    setPregunta(item.pregunta);
-  }
-
-  const handleClick = (item) => {
-    setModalIsOpen(true)
-    setContenidoModal(item.respuesta)
+  const validarRespuesta = () => {
+    if (pregunta.id === respuesta.id) {
+      setContenidoModal("¡Correcto!");
+      const nuevasPreguntasResueltas = [...preguntasResueltas, pregunta.id];
+      setPreguntasResueltas(nuevasPreguntasResueltas);
+      localStorage.setItem(
+        "preguntasResueltas",
+        JSON.stringify(nuevasPreguntasResueltas)
+      );
+    } else {
+      setContenidoModal("¡Incorrecto!");
+    }
   };
+
+  const handleClickRespuesta = (item) => {
+    if (pregunta === null) {
+      setContenidoModal("Primero escoge una pregunta");
+      setModalIsOpen(true);
+    } else {
+      setRespuesta(item);
+    }
+  };
+
+  useEffect(() => {
+    if (respuesta !== null) {
+      validarRespuesta();
+      setModalIsOpen(true);
+    }
+  }, [respuesta]);
 
   useEffect(() => {
     setPreguntasAleatorias(shuffleArray(data));
@@ -46,23 +64,77 @@ const Game = () => {
 
   return (
     <>
-      <Popup setModalIsOpen={setModalIsOpen} modalisOpen={modalisOpen}>
+      <Popup
+        setModalIsOpen={setModalIsOpen}
+        modalisOpen={modalisOpen}
+        pregunta={pregunta}
+        setPregunta={setPregunta}
+        setRespuesta={setRespuesta}
+      >
         {contenidoModal}
       </Popup>
+      <section className="p-2 max-w-7xl mx-auto text-2xl">
+        <div className="flex justify-between text-morado-abbott">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="cursor-pointer mr-4 h-8 w-8" // Añade margen a la izquierda para separarlo del otro SVG
+            onClick={() => {
+              setPregunta(null);
+              setRespuesta(null);
+            }}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M10 19l-7-7m0 0l7-7m-7 7h18"
+            />
+          </svg>
+          <svg
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            className="cursor-pointer h-8 w-8"
+            onClick={() => {
+              localStorage.removeItem("preguntasResueltas");
+              setPreguntasResueltas([]);
+              setPregunta(null);
+              setRespuesta(null);
+            }}
+          >
+            <path d="M23,12A11,11,0,1,1,12,1a10.9,10.9,0,0,1,5.882,1.7l1.411-1.411A1,1,0,0,1,21,2V6a1,1,0,0,1-1,1H16a1,1,0,0,1-.707-1.707L16.42,4.166A8.9,8.9,0,0,0,12,3a9,9,0,1,0,9,9,1,1,0,0,1,2,0Z" />
+          </svg>
+          
+        </div>
+      </section>
       <section className="p-2 space-y-16 max-w-7xl mx-auto text-2xl">
         <div>
           <h2 className="text-4xl text-violeta-abbott font-bold">A.</h2>
-          <ul className="grid-personalizado gap-4 my-6">
-            {data.map((item) => (
-              <li
-                key={item.id}
-                className="bg-violeta-abbott text-center p-6 rounded-lg cursor-pointer"
-                onClick={() => { selectPregunta(item) }}
-              >
-                {item.pregunta}
-              </li>
-            ))}
-          </ul>
+          {pregunta === null ? (
+            <ul className="grid-personalizado gap-4 my-6">
+              {data.map((item) => (
+                <li
+                  key={item.id}
+                  className={`bg-violeta-abbott text-center p-6 rounded-lg cursor-pointer ${
+                    preguntasResueltas.includes(item.id) ? "bg-green-500" : ""
+                  }`}
+                  onClick={() => {
+                    setPregunta(item);
+                    setRespuesta(null);
+                  }}
+                >
+                  {item.pregunta}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="bg-violeta-abbott p-6 rounded-lg">
+              {pregunta.pregunta}
+            </div>
+          )}
         </div>
         <div>
           <h2 className="text-4xl text-rosado-abbott font-bold">B.</h2>
@@ -70,8 +142,14 @@ const Game = () => {
             {preguntasAleatorias.map((item) => (
               <li
                 key={item.id}
-                className="bg-rosado-abbott p-6 rounded-lg cursor-pointer"
-                onClick={() => { selectRespuesta(item) }}
+                className={`bg-rosado-abbott p-6 rounded-lg ${
+                  pregunta === null ? "cursor-not-allowed" : "cursor-pointer"
+                } ${
+                  preguntasResueltas.includes(item.id) ? "bg-green-500" : ""
+                }`}
+                onClick={() => {
+                  handleClickRespuesta(item);
+                }}
               >
                 {item.respuesta}
               </li>
@@ -82,6 +160,5 @@ const Game = () => {
     </>
   );
 };
-
 
 export default Game;
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 
-const Popup = ({ setModalIsOpen, modalisOpen, children }) => {
+const Popup = ({ setModalIsOpen, modalisOpen, children, setPregunta, setRespuesta }) => {
   const modalRef = useRef(null);
 
   const handleClick = (e) => {
@@ -11,7 +11,11 @@ const Popup = ({ setModalIsOpen, modalisOpen, children }) => {
   };
 
   const cerrarModal = () => {
+    if (children === "¡Correcto!") {
+      setPregunta(null);
+    }
     setModalIsOpen(false);
+    setRespuesta(null);
   };
 
   return (
@@ -24,7 +28,10 @@ const Popup = ({ setModalIsOpen, modalisOpen, children }) => {
       } overflow-y-auto bg-morado-abbott/50 overflow-x-hidden fixed inset-0 z-50 justify-center items-center w-full md:inset-0 max-h-full cursor-pointer`}
       onClick={handleClick}
     >
-      <div className="relative p-4 w-full max-w-xl max-h-full cursor-default" ref={modalRef}>
+      <div
+        className="relative p-4 w-full max-w-xl max-h-full cursor-default"
+        ref={modalRef}
+      >
         <div className="relative bg-white rounded-lg shadow">
           <button
             type="button"
@@ -50,22 +57,41 @@ const Popup = ({ setModalIsOpen, modalisOpen, children }) => {
             <span className="sr-only">Close modal</span>
           </button>
           <div className="p-4 md:p-5 text-center">
-            <svg
-              className="mx-auto mb-4 text-morado-abbott w-12 h-12"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-              />
-            </svg>
-            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+            {children === "¡Correcto!" ? (
+              <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="mx-auto mb-4 text-green-500 w-12 h-12"
+              >
+                <path
+                  d="M16.0303 10.0303C16.3232 9.73744 16.3232 9.26256 16.0303 8.96967C15.7374 8.67678 15.2626 8.67678 14.9697 8.96967L10.5 13.4393L9.03033 11.9697C8.73744 11.6768 8.26256 11.6768 7.96967 11.9697C7.67678 12.2626 7.67678 12.7374 7.96967 13.0303L9.96967 15.0303C10.2626 15.3232 10.7374 15.3232 11.0303 15.0303L16.0303 10.0303Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M12 1.25C6.06294 1.25 1.25 6.06294 1.25 12C1.25 17.9371 6.06294 22.75 12 22.75C17.9371 22.75 22.75 17.9371 22.75 12C22.75 6.06294 17.9371 1.25 12 1.25ZM2.75 12C2.75 6.89137 6.89137 2.75 12 2.75C17.1086 2.75 21.25 6.89137 21.25 12C21.25 17.1086 17.1086 21.25 12 21.25C6.89137 21.25 2.75 17.1086 2.75 12Z"
+                  fill="currentColor"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="mx-auto mb-4 text-red-600 w-12 h-12"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+            )}
+
+            <h3 className="mb-5 text-3xl font-normal text-gray-600">
               {children}
             </h3>
             <button
